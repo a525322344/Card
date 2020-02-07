8064f3fae94185a1be35544276861dccb6d26a98
LxlNT <525322344@qq.com>
2020-02-07 17:00:36 +0800

    delayanimator

---
+++ Assets/Sprites/Real/delayAnimator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class delayAnimator : MonoBehaviour
+{
+    public float delayTime;
+
+    private Animator[] animators;
+    void Start()
+    {
+        animators = GetComponentsInChildren<Animator>();
+        if (animators.Length>0)
+        {
+            for(int i = 0; i < animators.Length; i++)
+            {
+                animators[i].speed = 0;
+            }
+            StartCoroutine(delayToAnima());
+        }
+    }
+
+    IEnumerator delayToAnima()
+    {
+        yield return new WaitForSeconds(delayTime);
+        for (int i = 0; i < animators.Length; i++)
+        {
+            animators[i].speed = 1;
+        }
+    }
+}
