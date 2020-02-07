e07a30fc8a7102a71cc160e4bc272eb82fe28263
LAPTOP-SA9G0UUC\77169 <771690820@qq.com>
2020-02-07 14:07:08 +0800

    美术资源更新/场景1/场景2

---
+++ Assets/ArtSources/hero/anim_play.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class anim_play : MonoBehaviour
+{
+    private Animator m_anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            m_anim.SetBool("attack", true);
+        }
+        else { m_anim.SetBool("attack", false); }
+        if (Input.GetKey(KeyCode.S))
+        {
+            m_anim.SetBool("defense", true);
+        }
+        else { m_anim.SetBool("defense", false); }
+        if (Input.GetKey(KeyCode.D))
+        {
+            m_anim.SetBool("hurted", true);
+        }
+        else { m_anim.SetBool("hurted", false); }
+    }
+}
---
+++ Assets/ArtSources/level01/scrips_ac/LlightShining.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LlightShining : MonoBehaviour
+{
+    private Light spotLight;
+    
+    void Start()
+    {
+        spotLight = gameObject.GetComponent<Light>();
+        
+    }
+
+    
+    void Update()
+    {
+        ShanShuo();
+    }
+    /// <summary>
+    /// 随着时间控制灯光强度
+    /// </summary>
+    void ShanShuo()
+    {
+        if (Time.time <= 0.3f)
+        {
+            spotLight.intensity = 5.0f;           
+        }
+        if (Time.time > 0.3f&& Time.time<0.5f)
+        {
+            spotLight.intensity = 15.0f;
+          
+        }
+        if (Time.time >= 0.5f&& Time.time<0.7f)
+        {
+            spotLight.intensity = 5.0f;
+        }
+        if (Time.time >= 0.7f && Time.time < 1.0f)
+        {
+            spotLight.intensity = 13.5f;
+        }
+        if (Time.time >= 1.0f&& Time.time<1.6f)
+        {
+            float intens = Mathf.Lerp(0.5f,17.0f,Random.Range(0.5f,0.9f));
+            spotLight.intensity = intens;
+        }
+        
+        if (Time.time >= 1.6f)
+        {
+            spotLight.intensity = 12.18f;           
+        }
+
+    }
+}
