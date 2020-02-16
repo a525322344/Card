adc64670b02b40b8b53fdf7e2c0cc716434c944e
LAPTOP-SA9G0UUC\77169 <771690820@qq.com>
2020-02-16 14:13:55 +0800

    更新cat的动画/一些小特效

---
+++ Assets/ArtSources/effect/attack/fire/firetest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firetest : MonoBehaviour
+{
+    public GameObject starPosition;//特效起始位置
+    public GameObject endPosition;//特效结束位置
+    public GameObject effect;//特效
+    private Vector3 x;
+
+   
+
+    void Start()
+    {
+        starPosition = gameObject;
+        endPosition = gameObject;
+        effect = gameObject;
+        x = endPosition.GetComponent<Transform>().position;
+
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            GameObject.Instantiate(effect,starPosition.gameObject.GetComponent<Transform>().transform);
+            effect.GetComponent<Transform>().Translate(x*1.0f);
+        }
+        if (effect.GetComponent<Transform>().position.x>endPosition.GetComponent<Transform>().position.x)
+        {
+            GameObject.Destroy(effect);
+        }
+    }
+}
