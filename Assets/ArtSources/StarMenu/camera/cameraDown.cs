3b6909a8cc1a77b50a124bfbdc310888dfee6a08
LAPTOP-SA9G0UUC\77169 <771690820@qq.com>
2020-03-07 19:39:51 +0800

    forest场景动画/starMenu初版/snowman动画

---
+++ Assets/ArtSources/StarMenu/camera/cameraDown.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraDown : MonoBehaviour
+{
+    private Animator camDown;
+
+    void Start()
+    {
+        camDown = gameObject.GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        //按左键摄像机往下拉
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("按下左键");
+            camDown.SetBool("down", true);
+        }
+    }
+}
+
